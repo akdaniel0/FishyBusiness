96d7e33009fc941c9a737bcfc78104fe2518ff28
JetpackHamster <jetpack.hamster.16@gmail.com>
2025-11-05 14:59:01 -0800

    upgrade affordability color indicators

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -5,10 +5,17 @@ public class GameManagerScript : MonoBehaviour
 {
     public float money;
     public float gameStartTime;
+    //public SidePanelScript sidepanel;
+    
+
+    public float profitIndicatorOpacity; // default 0, reset to 255 on profit earned
+    public float fadeSpeed; // opacity change rate
+    // TODO: child object/component for profitIndicator text here
 
     void Start()
     {
-        
+        //sidepanel = GameObject.Find("Canvas").GetComponent<SidePanelScript>();
+        // define child object/component for profitIndicator
     }
 
     // Update is called once per frame
@@ -16,6 +23,15 @@ public class GameManagerScript : MonoBehaviour
     {
         // update display rounded to tenth place
         gameObject.GetComponent<TextMeshProUGUI>().text = ("$" + Mathf.Round(money * 10f) / 10f);
+
+        // set floor for opacity
+        if (profitIndicatorOpacity < 0) {
+            profitIndicatorOpacity = 0;
+        }
+        // fade out indicator by decreasing opacity by fadeSpeed
+        while (profitIndicatorOpacity > 0) {
+            profitIndicatorOpacity -= Time.deltaTime * fadeSpeed;
+        }
         
     }
 }
--- Assets/Scripts/SidePanelScript.cs
+++ Assets/Scripts/SidePanelScript.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class SidePanelScript : MonoBehaviour
@@ -7,36 +8,54 @@ public class SidePanelScript : MonoBehaviour
     public GameManagerScript manager;
     public float[] upgradeCosts;
 
+    public Color32 baseTextColor;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         manager = GameObject.Find("Manager").GetComponent<GameManagerScript>();
         
-        transform.GetChild(2).GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(doUpgrade1);
+        transform.GetChild(4).GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(doUpgrade1);
         transform.GetChild(3).GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(doUpgrade2);
-        transform.GetChild(4).GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(doUpgrade3);
+        transform.GetChild(2).GetChild(0).gameObject.GetComponent<Button>().onClick.AddListener(doUpgrade3);
+
+        transform.GetChild(2).GetChild(0).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().overrideColorTags = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // update top button text color to red if cannot afford that upgrade
+        if (manager.money <= upgradeCosts[0]) {
+            transform.GetChild(4).GetChild(0).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().color = new Color32(255, 0, 0, 255);
+        } else {
+            transform.GetChild(4).GetChild(0).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().color = new Color32(255, 255, 255, 255);
+        }
+        // color update for middle button
+        if (manager.money <= upgradeCosts[1]) {
+            transform.GetChild(3).GetChild(0).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().color = new Color32(255, 0, 0, 255);
+        } else {
+            transform.GetChild(3).GetChild(0).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().color = new Color32(255, 255, 255, 255);
+        }
+        // color update for bottom button
+        if (manager.money <= upgradeCosts[2]) {
+            transform.GetChild(2).GetChild(0).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().color = new Color32(255, 0, 0, 255);
+        } else {
+            transform.GetChild(2).GetChild(0).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().color = new Color32(255, 255, 255, 255);
+        }
     }
 
     // attempt to buy upgrade
     void doUpgrade1() {
-        if (manager.money > upgradeCosts[0]) {
-            GameObject.Find("FishTankCraneCenter").GetComponent<FishTankCraneScript>().craneAcceleration *= 1.1f;
-            GameObject.Find("FishTankCraneCenter").GetComponent<FishTankCraneScript>().craneDrag *= 1.05f;
-            GameObject.Find("ConveyorCraneCenter").GetComponent<ConveyorCraneScript>().craneAcceleration *= 1.1f;
-            GameObject.Find("ConveyorCraneCenter").GetComponent<ConveyorCraneScript>().craneDrag *= 1.05f; 
-            manager.money -= upgradeCosts[0];
+        if (manager.money >= upgradeCosts[2]) {
+            GameObject.Find("ConveyorA").GetComponent<ConveyorAScript>().conveyorSpeed *= 0.9f;
+            manager.money -= upgradeCosts[2];
         }
     }
     
     // attempt to buy upgrade
     void doUpgrade2() {
-        if (manager.money > upgradeCosts[1]) {
+        if (manager.money >= upgradeCosts[1]) {
             GameObject.Find("FishTankCraneCenter").GetComponent<FishTankCraneScript>().verticalSpeed *= 1.25f;
             manager.money -= upgradeCosts[1];
         }
@@ -44,9 +63,12 @@ public class SidePanelScript : MonoBehaviour
     
     // attempt to buy upgrade
     void doUpgrade3() {
-        if (manager.money > upgradeCosts[2]) {
-            GameObject.Find("ConveyorA").GetComponent<ConveyorAScript>().conveyorSpeed *= 0.9f;
-            manager.money -= upgradeCosts[2];
+        if (manager.money >= upgradeCosts[0]) {
+            GameObject.Find("FishTankCraneCenter").GetComponent<FishTankCraneScript>().craneAcceleration *= 1.1f;
+            GameObject.Find("FishTankCraneCenter").GetComponent<FishTankCraneScript>().craneDrag *= 1.05f;
+            GameObject.Find("ConveyorCraneCenter").GetComponent<ConveyorCraneScript>().craneAcceleration *= 1.1f;
+            GameObject.Find("ConveyorCraneCenter").GetComponent<ConveyorCraneScript>().craneDrag *= 1.05f; 
+            manager.money -= upgradeCosts[0];
         }
     }
 }
