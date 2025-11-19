5b473d9ff572e8a5fc77009a68836139165ca3d0
JetpackHamster <jetpack.hamster.16@gmail.com>
2025-11-19 13:46:04 -0800

    conveyorscript arrow child object support

--- Assets/Scripts/ConveyorAScript.cs
+++ Assets/Scripts/ConveyorAScript.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class ConveyorAScript : MonoBehaviour
 {
     public GameObject[] grabbedObjs;
+    GameObject arrowVisual;
 
     public float conveyorSpeed;
     public Animator C_animation;
+    public bool hasArrowChild;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         grabbedObjs = new GameObject[0];
         C_animation.speed = Mathf.Abs(conveyorSpeed) * 1.95f;
+        
+        if (hasArrowChild) {
+            arrowVisual = transform.GetChild(0);
+        }
     }
 
     // Update is called once per frame
@@ -21,7 +27,10 @@ public class ConveyorAScript : MonoBehaviour
             grabbedObjs[i].transform.position = new Vector3(grabbedObjs[i].transform.position.x + (-conveyorSpeed * Time.deltaTime), grabbedObjs[i].transform.position.y, grabbedObjs[i].transform.position.z);
         }
 
-        // move conveyor visual
+        // move conveyor visual if enabled
+        if (hasArrowChild) {
+            arrowVisual.transform.position = new Vector3(arrowVisual.transform.position.x + (-conveyorSpeed * Time.deltaTime), arrowVisual.transform.position.y, arrowVisual.transform.position.z);
+        }
         
     }
     // detect collisions
