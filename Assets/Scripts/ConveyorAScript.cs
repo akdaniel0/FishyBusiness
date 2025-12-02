f14959f9ecba5b9205b3e7cc9a179f149aee441f
gavdevelop1232 <93094796+gavdevelop1232@users.noreply.github.com>
2025-12-02 14:53:16 -0800

    Made Conveyor Belt Smooth, and other little improvments

--- Assets/Scripts/ConveyorAScript.cs
+++ Assets/Scripts/ConveyorAScript.cs
@@ -8,21 +8,25 @@ public class ConveyorAScript : MonoBehaviour
     public float conveyorSpeed;
     public Animator C_animation;
     public bool hasArrowChild;
+    private Vector3 ogpos;
+    public float bounds;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         grabbedObjs = new GameObject[0];
-        C_animation.speed = Mathf.Abs(conveyorSpeed) * 1.95f;
+        //C_animation.speed = Mathf.Abs(conveyorSpeed) * 1.95f;
         
         if (hasArrowChild) {
             arrowVisual = transform.GetChild(0).gameObject;
+            this.ogpos = arrowVisual.transform.position;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(GameObject.Find("Canvas").GetComponent<Canvas>().enabled) { return; }
         for (int i = 0; i < grabbedObjs.Length; i++) {
             // remove deleted fish from array
             if (grabbedObjs[i] == null) {
@@ -32,9 +36,16 @@ public class ConveyorAScript : MonoBehaviour
             }
         }
 
+        //this.conveyor_head.localPosition = Vector3.MoveTowards(this.conveyor_head.localPosition, new Vector3(-0.882f, 0f), this.conveyorSpeed * 0.05f);
+        
+
         // move conveyor visual if enabled
         if (hasArrowChild) {
             arrowVisual.transform.position = new Vector3(arrowVisual.transform.position.x + (-conveyorSpeed * Time.deltaTime), arrowVisual.transform.position.y, arrowVisual.transform.position.z);
+            if ((this.conveyorSpeed > 0f && arrowVisual.transform.localPosition.x <= this.bounds) || (this.conveyorSpeed < 0f && arrowVisual.transform.localPosition.x >= this.bounds))
+            {
+                this.arrowVisual.transform.position = this.ogpos;
+            }
         }
         
     }
